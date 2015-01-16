90724428e0b42b129609c4311e4d6bdb5761ad16
Mariusz Sielski <mariusz.sielski@itti.com.pl>
2015-01-16 08:40:41 +0100

    xx

--- www/js/phone-call/controllers/call.controler.js
+++ www/js/phone-call/controllers/call.controler.js
@@ -1,11 +1,11 @@
 angular.module('starter.phone-call').controller('CallController', function ($window) {
   this.makeCall = function (phoneNumber) {
-    if (!$window.ionic.Platform.isIOS()) {
+    //if (!$window.ionic.Platform.isIOS()) {
       $window.document.location.href = 'tel:' + phoneNumber;
-    } else {
-      console.log("IOS call: " + phoneNumber);
-      $window.document.location.href = 'telprompt://' + phoneNumber;
-      //$window.phoneDialer.dial(phoneNumber);
-    }
+    //} else {
+    //  console.log("IOS call: " + phoneNumber);
+    //  $window.document.location.href = 'tel:' + phoneNumber;
+    //  //$window.phoneDialer.dial(phoneNumber);
+    //}
   };
 });
